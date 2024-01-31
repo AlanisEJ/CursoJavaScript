f8a6116d3d140b2c5d3a90da39dc7db7ed8d2c9e
AlanisEJ <naninnano12@hotmail.com>
2024-01-31 09:29:57 -0700

    Agregue nuevo avance del cursor

---
+++ loopFor/loopfor.js
@@ -0,0 +1,13 @@
+function multi(){
+    //Obtener el numero 
+    let numero = document.getElementById("numero")
+    let textoNumeroTabla = nuemro.value
+    let numeroTabla = Number(textoNumeroTabla)
+    
+   
+    for (let index = 0; index < ar
+        ray.length; index++) {
+        const element = array[index];
+        
+    }
+}
